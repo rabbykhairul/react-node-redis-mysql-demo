c1f3bb44db064789a58a3df6f6feeb4ff890d44c
rabbykhairul <rabbykhairul@gmail.com>
2023-06-08 17:40:22 +0600

    create react app

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header style={{ width: "100vw", display: "flex", alignItems: "center", flexDirection: "column" }}>
+        <h1>Country Map</h1>
+        <hr style={{ width: "100%" }} />
+      </header>
+    </>
+  );
+}
+
+export default App;
