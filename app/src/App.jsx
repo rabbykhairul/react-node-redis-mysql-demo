d8b31054d40affc9d416fedddaed4b3f0077d8ee
rabbykhairul <rabbykhairul@gmail.com>
2023-06-08 23:08:26 +0600

    display selected country details

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import classnames from "classnames";
 
 const BASE_URL = "http://localhost:4000";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [country, setCountry] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    if (!selected) return;
+
+    setIsLoading(true);
+    axios
+      .get(`${BASE_URL}/countries/${selected}`)
+      .then(({ data }) => {
+        setCountry(data);
+      })
+      .finally(() => setIsLoading(false));
+  }, [selected]);
+
+  useEffect(() => {
+    console.table(["effect"]);
     axios
       .get(`${BASE_URL}/countries`)
       .then(({ data }) => {
@@ -36,12 +53,12 @@ function App() {
         <section>Foo</section>
         <section>
           {countries.map((c, idx) => (
-            <div className="list" key={idx}>
+            <div className={classnames("list", { selected: c === selected })} key={idx} onClick={() => setSelected(c)}>
               {c}
             </div>
           ))}
         </section>
-        <section>Foo</section>
+        <section>{isLoading ? <h3>Loading...</h3> : <pre>{JSON.stringify(country, null, 2)}</pre>}</section>
       </div>
     </>
   );
