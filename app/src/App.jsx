d71b24ea2c6d217e8ccfd09b148fdd539782a856
rabbykhairul <rabbykhairul@gmail.com>
2023-06-08 22:14:55 +0600

    setup basic app layout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,9 +5,14 @@ function App() {
   return (
     <>
       <header style={{ width: "100vw", display: "flex", alignItems: "center", flexDirection: "column" }}>
-        <h1>Country Map</h1>
+        <h1>Country List</h1>
         <hr style={{ width: "100%" }} />
       </header>
+      <div style={{ display: "flex" }}>
+        <section>Foo</section>
+        <section>Foo</section>
+        <section>Foo</section>
+      </div>
     </>
   );
 }
